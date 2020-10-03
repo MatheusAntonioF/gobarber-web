475f9fca2bcdabbe0e1b38bf0718cd4b1a2cb4ac
Matheus Antonio <matheusantoniof@gmail.com>
2020-10-03 00:04:51 -0300

    feat: create global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import GlobalStyles from './styles/global';
 
 const App: React.FC = () => {
-  return <h1>Hello world</h1>;
+  return (
+    <>
+      <GlobalStyles />
+      <h1>Hello world</h1>
+    </>
+  );
 };
 
 export default App;
