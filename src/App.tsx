4cd93dd0f0e30ea07b5a37c7b092511f93f77b66
Matheus Antonio <matheusantoniof@gmail.com>
2020-10-04 00:19:59 -0300

    feat: signup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import GlobalStyles from './styles/global';
-import Signin from './pages/Signin';
+// import Signin from './pages/Signin';
+import SignUp from './pages/SignUp';
 
 const App: React.FC = () => {
   return (
     <>
       <GlobalStyles />
-      <Signin />
+      <SignUp />
     </>
   );
 };
